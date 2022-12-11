c0dcdf8aace11bd6e4aaba61510acc5fdf19f238
realing29 <realing29@gmail.com>
2022-12-11 06:59:44 +0500

    validation form authorization

--- client/src/components/page/login/loginPage.jsx
+++ client/src/components/page/login/loginPage.jsx
@@ -1,26 +1,53 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import TextField from '../../common/form/textField'
+import * as yup from 'yup'
 
 const LoginPage = () => {
 	const [data, setData] = useState({
-		login: '',
+		email: '',
 		password: '',
 		remember: false,
 	})
+	const [errors, setErrors] = useState({})
+
+	const validateShema = yup.object().shape({
+		password: yup.string().required('Пароль обязателен для заполнения'),
+		email: yup
+			.string()
+			.required('Электронная почта обязательна для заполнения')
+			.email('Email введен не корректно'),
+	})
+
+	const validate = async () => {
+		try {
+			await validateShema.validate(data)
+			setErrors({})
+			return true
+		} catch (err) {
+			setErrors({ [err.path]: err.message })
+			return false
+		}
+	}
 
 	const handleChange = ({ name, value }) => {
 		setData((prev) => ({ ...prev, [name]: value }))
 	}
 
-	const handleSubmit = () => {
-		console.log(data)
+	useEffect(() => {
+		validate()
+	}, [data])
+
+	const handleSubmit = async () => {
+		const isValid = await validate()
+		console.log(data, errors)
+		if (!isValid) return
 	}
 
 	return (
 		<>
 			<h1>Авторизация</h1>
-			<TextField label='Логин' value={data.login} name='login' onChange={handleChange} />
+			<TextField label='Логин' value={data.email} name='email' onChange={handleChange} />
 			<TextField
 				label='Пароль'
 				value={data.password}
@@ -30,7 +57,16 @@ const LoginPage = () => {
 			/>
 
 			<label htmlFor='remember'>
-				<input type='checkbox' name='remember' id='remember' />
+				<input
+					type='checkbox'
+					name='remember'
+					id='remember'
+					onChange={({ target }) => {
+						handleChange({ name: target.name, value: target.checked })
+					}}
+					value='remember'
+					checked={data.remember}
+				/>
 				Оставаться в системе
 			</label>
 			<button type='button' className='btn_design' onClick={handleSubmit}>
--- client/src/components/page/registration/registrationPage.jsx
+++ client/src/components/page/registration/registrationPage.jsx
@@ -1,22 +1,64 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import TextField from '../../common/form/textField'
+import * as yup from 'yup'
 
 const RegistrationPage = () => {
 	const [data, setData] = useState({
-		login: '',
+		email: '',
 		password: '',
 		repeatPassword: '',
+		confirm: false,
 	})
+
+	const [errors, setErrors] = useState({})
+
+	const validateShema = yup.object().shape({
+		password: yup
+			.string()
+			.min(8, 'Минимум 8 символов')
+			.required('Пароль обязателен для заполнения'),
+		repeatPassword: yup
+			.string()
+			.test('equal', 'Пароли не совпадают', (value) => value === data.password)
+			.required('Пароль обязателен для заполнения'),
+		email: yup
+			.string()
+			.required('Электронная почта обязательна для заполнения')
+			.email('Email введен не корректно'),
+		confirm: yup
+			.boolean()
+			.test('true', 'Вы должны согласиться с правилами', (value) => value === true),
+	})
+
+	const validate = async () => {
+		try {
+			await validateShema.validate(data)
+			setErrors({})
+			return true
+		} catch (err) {
+			setErrors({ [err.path]: err.message })
+			return false
+		}
+	}
+	useEffect(() => {
+		validate()
+	}, [data])
 
 	const handleChange = ({ name, value }) => {
 		setData((prev) => ({ ...prev, [name]: value }))
 	}
 
+	const handleSubmit = async () => {
+		const isValid = await validate()
+		console.log(data, errors)
+		if (!isValid) return
+	}
+
 	return (
 		<>
 			<h1>Регистрация</h1>
-			<TextField label='Логин' value={data.login} name='login' onChange={handleChange} />
+			<TextField label='Логин' value={data.email} name='email' onChange={handleChange} />
 			<TextField
 				label='Пароль'
 				value={data.password}
@@ -32,11 +74,20 @@ const RegistrationPage = () => {
 				type='password'
 			/>
 
-			<label htmlFor='remember'>
-				<input type='checkbox' name='remember' id='remember' />
+			<label htmlFor='confirm'>
+				<input
+					type='checkbox'
+					name='confirm'
+					id='confirm'
+					value='confirm'
+					checked={data.confirm}
+					onChange={({ target }) => {
+						handleChange({ name: target.name, value: target.checked })
+					}}
+				/>
 				Принимаю пользовательское соглашение
 			</label>
-			<button type='button' className='btn_design'>
+			<button type='button' className='btn_design' onClick={handleSubmit}>
 				Зарегистрироваться
 			</button>
 			<Link to='/login'>Войти</Link>
